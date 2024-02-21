85f34e4ca7f30f57237bf1206f4d482b66d2cd19
socrate-01 <seydinamouhamadousanoumbaye@esp.sn>
2024-02-20 20:50:46 -0500

    Cookies new

--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -1,49 +1,83 @@
-// CookieConsent.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 
 const CookieConsent = () => {
-  const [cookies, setCookie] = useCookies(['user-consent']);
+  const [cookies, setCookie] = useCookies([
+    'user-consent',
+    'session-cookies',
+    'persistent-cookies',
+    'essential-cookies',
+    'performance-cookies',
+    'functionality-cookies',
+    'tracking-cookies'
+  ]);
+  
+  // Utilisation de useState pour définir la visibilité initiale basée sur sessionStorage
+  const [isVisible, setIsVisible] = useState(!sessionStorage.getItem('hideCookieConsent'));
 
-  if (cookies['user-consent']) {
-    return null; // L'utilisateur a déjà accepté, ne rien afficher.
-  }
+  useEffect(() => {
+    if (cookies['user-consent']) {
+      // Si l'utilisateur a déjà accepté les cookies, ne pas afficher le consentement et stocker dans sessionStorage
+      sessionStorage.setItem('hideCookieConsent', 'true');
+    }
+  }, [cookies]); // Se déclenche lorsque les cookies changent
 
   const handleAccept = () => {
-    setCookie('user-consent', 'accepted', { path: '/', maxAge: 2592000 }); // 30 jours
+    const commonOptions = { path: '/', maxAge: 2592000 }; // 30 jours pour l'exemple
+    setCookie('user-consent', 'accepted', commonOptions);
+    setCookie('session-cookies', 'accepted', commonOptions);
+    setCookie('persistent-cookies', 'accepted', commonOptions);
+    setCookie('essential-cookies', 'accepted', commonOptions);
+    setCookie('performance-cookies', 'accepted', commonOptions);
+    setCookie('functionality-cookies', 'accepted', commonOptions);
+    setCookie('tracking-cookies', 'accepted', commonOptions);
+    sessionStorage.setItem('hideCookieConsent', 'true');
+    setIsVisible(false);
   };
+
+  const handleDecline = () => {
+    sessionStorage.setItem('hideCookieConsent', 'true');
+    setIsVisible(false);
+  };
+
+  if (!isVisible) {
+    return null;
+  }
 
   return (
     <div style={{
       position: 'fixed',
-      top: '50%', // Centré verticalement
-      left: '50%', // Centré horizontalement
-      transform: 'translate(-50%, -50%)', // Ajuste le centrage
-      backgroundColor: '#fff', // Fond blanc pour le popup
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: '#fff',
       padding: '40px',
-      zIndex: 10000, // Assure que le popup est bien au-dessus des autres éléments
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Ombre pour un effet de profondeur
-      borderRadius: '10px', // Bords arrondis pour le popup
-      maxWidth: '500px', // Largeur maximale du popup
-      width: '90%', // Adaptatif selon la largeur de l'écran
-      textAlign: 'center', // Centre le texte
-      fontFamily: 'Arial, sans-serif', // Police de caractères
+      zIndex: 10000,
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+      borderRadius: '10px',
+      maxWidth: '500px',
+      width: '90%',
+      textAlign: 'center',
+      fontFamily: 'Arial, sans-serif',
     }}>
+      <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+        <a href="#" onClick={handleDecline} style={{ color: '#db1262', textDecoration: 'none', fontWeight: 'bold' }}>Continuer sans accepter</a>
+      </div>
       <h2 style={{ color: '#343a40', margin: '0 0 20px 0' }}>Consentement aux Cookies</h2>
       <p style={{ margin: '0 0 20px 0', fontSize: '16px', lineHeight: '1.5', textAlign: 'left' }}>
-      Kiess Inc et ses partenaires utilisent des cookies pour améliorer votre expérience, analyser le trafic, et personnaliser les publicités. En utilisant notre site, vous acceptez notre utilisation de cookies pour ces objectifs. Vous pouvez choisir d'accepter, personnaliser vos préférences ou naviguer sans accepter les cookies non essentiels. Vos choix sont modifiables à tout moment via les paramètres de votre compte. Vous pouvez voir nos 
-        <a href="/Conditions" style={{ color: '#db1262', textDecoration: 'none', fontWeight: 'bold' }}> politique de cookies</a>.
+        Kiess Inc et ses partenaires utilisent des cookies pour améliorer votre expérience, analyser le trafic, et personnaliser les publicités. En utilisant notre site, vous acceptez notre utilisation de cookies pour ces objectifs. Vous pouvez choisir d'accepter, personnaliser vos préférences ou naviguer sans accepter les cookies non essentiels. Vos choix sont modifiables à tout moment via les paramètres de votre compte. Vous pouvez voir nos 
+        <a href="/Conditions" style={{ color: '#db1262', textDecoration: 'none', fontWeight: 'bold' }}>politique de cookies</a>.
       </p>
       <button onClick={handleAccept} style={{
-        backgroundColor: '#db1262', // Couleur du bouton
+        backgroundColor: '#db1262',
         color: 'white',
         border: 'none',
         padding: '12px 25px',
         cursor: 'pointer',
-        borderRadius: '5px', // Bords arrondis pour le bouton
-        fontSize: '18px', // Taille de la police du bouton
+        borderRadius: '5px',
+        fontSize: '18px',
         fontWeight: 'bold',
-        boxShadow: '0 2px 5px rgba(0,0,0,0.2)' // Ombre portée pour le bouton
+        boxShadow: '0 2px 5px rgba(0,0,0,0.2)'
       }}>Accepter</button>
     </div>
   );
